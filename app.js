54ae8d007f5cef089b5309cac6853c839b889cbf
Idnan Tariq <Idnan79@gmail.com>
2021-04-14 14:35:08 +0200

    newupdate

---
+++ app.js
@@ -0,0 +1,13 @@
+function  favTutorial() {
+    var list = document.getElementById("list");
+    //document.getElementById("favourite").value = list.options[list.selectedIndex].text;
+    document.getElementById("favourite").value = list.options[list.selectedIndex].value;
+    }
+
+  /*var select = document.getElementById('list');
+  var input = document.getElementById('txtprice');
+                select.onchange = function() {
+
+                    input.value = select.value; 
+
+  }  */
